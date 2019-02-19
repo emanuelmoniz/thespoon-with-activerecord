c45fe1170973b1f81e023e0b4040f4cb3647b065
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-02-19 11:30:53 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index'
+
+  get "restaurants/new", to: "restaurants#new", as: "restaurant_new"
+  post "restaurants", to: "restaurants#create"
+  # The `show` route needs to be *after* `new` route.
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  get "restaurants/:id/edit", to: "restaurants#edit", as: "restaurant_edit"
+  patch "restaurants/:id", to: "restaurants#update"
+
+  delete "restaurants/:id", to: "restaurants#destroy", as: "restaurant_delete"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_091603) do
+ActiveRecord::Schema.define(version: 2019_02_19_102239) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "stars"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
